fdac2fae6975a01ae2606ee1c751d0f40534b642
wlad9 <zav8b9@gmail.com>
2023-09-03 18:39:48 -0300

    Projeto base criado

---
+++ src/main/java/com/desafio5/dscommer_v3/DscommerV3Application.java
@@ -0,0 +1,13 @@
+package com.desafio5.dscommer_v3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscommerV3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscommerV3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio5/dscommer_v3/DscommerV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio5.dscommer_v3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscommerV3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
